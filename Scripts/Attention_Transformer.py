4d4c54b13cd9691b839b5bfdec07a1b515216ab2
Anoop CA <anoopafter@gmail.com>
2024-05-20 07:19:02 +0530

    Code updates

--- Scripts/Attention_Transformer.py
+++ Scripts/Attention_Transformer.py
@@ -1,6 +1,8 @@
 #import necessary libraries
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+import keras
 
 df = pd.read_csv(r'D:\ML_Projects\AI_Tech_ChatBot\Data\UbuntuDialogueText.csv')
 
