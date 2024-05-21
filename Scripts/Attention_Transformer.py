ea7b6ce15a44ea0e964601e8a36522ea69384782
Anoop CA <anoopafter@gmail.com>
2024-05-22 05:02:17 +0530

    Modeling updates

--- Scripts/Attention_Transformer.py
+++ Scripts/Attention_Transformer.py
@@ -2,7 +2,25 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+import joblib
+import warnings
 
-df = pd.read_csv(r'D:\ML_Projects\AI_Tech_ChatBot\Data\UbuntuDialogueText.csv')
+warnings.filterwarnings("ignore")
 
+class Transformer:
+    def __init__(self):
+        self.model = self.build_model()
+    
+    def build_model(self):
+        model = Sequential()
+
+        return model
+    
+    def attention(self):
+
+        return model
+    
