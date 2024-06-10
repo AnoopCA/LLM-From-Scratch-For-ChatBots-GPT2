4cf563d90741a2124bdf5a016fc219d6e68d5a31
Anoop CA <anoopafter@gmail.com>
2024-06-10 21:33:18 +0530

    Modeling updates

--- Scripts/word2vec.py
+++ Scripts/word2vec.py
@@ -17,17 +17,26 @@ def get_word_vector(word):
 gpt_data = pd.read_csv(r'D:\ML_Projects\AI_Tech_ChatBot\Data\ChatGPT_chatlogs\GPT_chatlogs_all.csv')
 
 vec = []
-print_lim = 0
+#print_lim = 0
 for i in range(len(gpt_data)):
-    if print_lim == 1:
-        break
+#    if print_lim == 2:
+#        break
     words = gpt_data['Question'][i].split()
     vec.append([get_word_vector(w) for w in words])
     #list(map(lambda w: get_word_vector(w), words))
-    print_lim+=1
-    
+#    print_lim+=1
 
-print("actual word: ", gpt_data['Question'][i].split()[1],
-      "Predicted word: ", word2vec_model.similar_by_vector(get_word_vector(gpt_data['Question'][i].split()[1]), topn=1)[0][0])
+for i in range(len(gpt_data)):
+    for j in vec[i]:
+        print(word2vec_model.similar_by_vector(j, topn=1)[0][0])
 
-# try loop through the end of the vector and print the output sentence
+#for i in range(len(gpt_data)):
+#    for j in range(len(gpt_data['Question'][i].split())):
+#        print(word2vec_model.similar_by_vector(vec[i][j], topn=1)[0][0])
+
+#for j in range(len(gpt_data)):
+#    print("actual word: ", gpt_data['Question'][j])
+#    print("Predicted word: ")
+#    for k in get_word_vector(gpt_data['Question'][i].split()[1]):
+#        print("k:", k)
+        #print(word2vec_model.similar_by_vector(k, topn=1))
