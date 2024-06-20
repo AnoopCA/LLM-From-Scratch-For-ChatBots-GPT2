4cd0053b856c7335ef508a09f4e8d684a9ce7ce0
Anoop CA <anoopafter@gmail.com>
2024-06-21 03:40:34 +0530

    Modeling updates - tiktoken

--- Scripts/LLM_run.py
+++ Scripts/LLM_run.py
@@ -1,12 +1,14 @@
 import torch
 import sys
+import tiktoken
 
 sys.path.append(r'D:\ML_Projects\AI_Tech_ChatBot\Scripts')
-from LLM_Finetune_Char import Head, MultiHeadAttention, FeedFoward, Block, GPTLanguageModel, encode, decode, string_to_int, int_to_string
+from LLM_Finetune_Tiktoken import Head, MultiHeadAttention, FeedFoward, Block, GPTLanguageModel
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+tokenizer = tiktoken.get_encoding("cl100k_base")
 
-model = torch.load(r'D:\ML_Projects\AI_Tech_ChatBot\Models\model-06.pth')
+model = torch.load(r'D:\ML_Projects\AI_Tech_ChatBot\Models\model-09_loss-0.770.pth') #model-06.pth')
 model.eval()
 model.to(device)
 
@@ -14,6 +16,6 @@ while True:
     prompt = input("Prompt (enter 'x' to exit):\n")
     if prompt.lower() == "x":
         break
-    context = torch.tensor(encode(prompt), dtype=torch.long, device=device)
-    generated_chars = decode(model.generate(context.unsqueeze(0), max_new_tokens=150)[0].tolist())
+    context = torch.tensor(tokenizer.encode(prompt), dtype=torch.long, device=device)
+    generated_chars = tokenizer.decode(model.generate(context.unsqueeze(0), max_new_tokens=150)[0].tolist())
     print(f'Generated text:\n{generated_chars}')
