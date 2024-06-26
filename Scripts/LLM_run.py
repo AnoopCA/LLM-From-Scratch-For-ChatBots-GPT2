b66533d4be5cc94fb0076024f1f23efc9df8f050
Anoop CA <anoopafter@gmail.com>
2024-06-27 01:48:12 +0530

    Model training

--- Scripts/LLM_run.py
+++ Scripts/LLM_run.py
@@ -1,3 +1,6 @@
+
+#LLM-From-Scratch-For-ChatBots-GPT2
+
 import torch
 import sys
 import tiktoken
@@ -8,7 +11,7 @@ from LLM_Finetune_Tiktoken import Head, MultiHeadAttention, FeedFoward, Block, G
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 tokenizer = tiktoken.get_encoding("cl100k_base")
 
-model = torch.load(r'D:\ML_Projects\AI_Tech_ChatBot\Models\model-21_loss-0.576.pth')
+model = torch.load(r'D:\ML_Projects\AI_Tech_ChatBot\Models\model-23_loss-0.181.pth')
 #model = torch.load(r'D:\ML_Projects\AI_Tech_ChatBot\Models\model_checkpoints\model_temp_275_loss-1.209.pth')
 model.eval()
 model.to(device)
@@ -19,6 +22,7 @@ while True:
         break
     #prompt = " [SEP] Question: " + str(prompt) + " [SEP] Answer: "
     #prompt = " [SEP] " + prompt #+ " [SEP] "
+    prompt = prompt + " [SEP] "
     context = torch.tensor(tokenizer.encode(prompt), dtype=torch.long, device=device)
-    generated_chars = tokenizer.decode(model.generate(context.unsqueeze(0), max_new_tokens=10)[0].tolist())
+    generated_chars = tokenizer.decode(model.generate(context.unsqueeze(0), max_new_tokens=250)[0].tolist())
     print(f'Generated text:\n{generated_chars}')
